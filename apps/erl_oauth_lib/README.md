61bac434500348abd08af6e715f06e3e7cbb656b
wushuang <wushuang@dayukeji.com>
2021-01-09 11:40:06 +0800

    fileMode

--- apps/erl_oauth_lib/README.md
+++
@@ -1,9 +0,0 @@
-erl_oauth_lib
-=====
-
-通用个平台验证库
-
-Build
------
-
-    $ rebar3 compile
--- apps/erl_oauth_lib/src/erl_oauth_platfrom_error.erl
+++
@@ -1,9 +0,0 @@
--module(erl_oauth_platfrom_error).
-
--behaviour(erl_oauth_platform).
-
--export([auth/1]).
-
--spec auth(any()) -> {error, unsupport}.
-auth(_) ->
-    {error, unsupport}.
